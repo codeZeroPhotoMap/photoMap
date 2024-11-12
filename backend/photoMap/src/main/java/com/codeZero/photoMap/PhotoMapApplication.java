929b657540cafd91efc3d36f41d19fe20c35fb48
ghd04001 <ghd04001@gmail.com>
2024-11-12 16:27:45 +0900

    [BE] chore: 프로젝트 Timezone 변경(2)

--- backend/photoMap/src/main/java/com/codeZero/photoMap/PhotoMapApplication.java
+++ backend/photoMap/src/main/java/com/codeZero/photoMap/PhotoMapApplication.java
@@ -1,8 +1,11 @@
 package com.codeZero.photoMap;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -10,6 +13,11 @@ public class PhotoMapApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhotoMapApplication.class, args);
+	}
+
+	@PostConstruct
+	public void setTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
